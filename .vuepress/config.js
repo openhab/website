596d7ff7815bbabd2fdf86a273e65f958fe9d796
Yannick Schaus <github@schaus.net>
2018-04-29 21:28:05 +0200

    Update vuepress, add cookie consent & netlify identity widget
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,6 +9,8 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: `/openhab-logo-square.png` }],
     ['link', { rel: 'stylesheet', href: `//fonts.googleapis.com/css?family=Open+Sans:300,400` }]
+    ['script', { src: `https://identity.netlify.com/v1/netlify-identity-widget.js` }]
+    ['script', { src: `https://cookieinfoscript.com/js/cookieinfo.min.js` }]
   ],
   serviceWorker: false,
   themeConfig: {
