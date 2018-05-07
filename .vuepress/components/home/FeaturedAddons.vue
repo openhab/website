1f2ff938b4e31ca08ff7ad773a6a15e1b8721f70
Yannick Schaus <github@schaus.net>
2018-05-07 16:53:04 +0200

    Download instructions wizard
    Add content to "who we are" and "contribute" pages.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/components/home/FeaturedAddons.vue
+++ .vuepress/components/home/FeaturedAddons.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="section3">
+  <div class="featured-addons-section">
     <h2>More than 1000 supported things!</h2>
     <div class="logos">
       <router-link :to="addon.path" v-for="addon in featuredAddons" :key="addon.path" class="logo-container">
         <img :src="addon.frontmatter.logo.replace('images/addons/', '/logos/')" class="featured-logo" />
       </router-link>
     </div>
+    <div class="addons-button-container">
+      <router-link to="addons" class="all-addons-button slide">See All Add-ons →</router-link>
+    </div>
   </div>  
 </template>
 
 <style lang="stylus">
-.section3
+.featured-addons-section
   background white
   display flex
   flex-direction column
@@ -45,6 +48,21 @@
         .featured-logo
           max-height 90px
           max-width 90px
+  .addons-button-container
+    padding 2rem
+    text-align center
+  .all-addons-button
+    display inline-block
+    font-size 1rem
+    color #ff6600
+    padding 0.8rem 1.6rem
+    border-radius 4px
+    transition background-color .1s ease
+    box-sizing border-box
+    border 1px solid #ff6600
+    &:hover
+      background-color #ff6600
+      color white
 
 </style>
 
--- about/events.md
+++ about/events.md
@@ -4,4 +4,10 @@ layout: AboutPage
 title: Events
 ---
 
-tbd
+If you are organizing an event around openHAB, please let us know in the [Events category](https://community.openhab.org/c/organisation/events) of the community forum. We will work with you to add it on this page.
+
+---
+
+<!-- TODO: list component and event page layout -->
+
+_No upcoming events._
