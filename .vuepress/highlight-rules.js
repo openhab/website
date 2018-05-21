fca7a654500c59e35677e17d195fea3bdbe4e5e0
Yannick Schaus <github@schaus.net>
2018-05-22 00:35:17 +0200

    Improve var/val highlighting in rules
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/highlight-rules.js
+++ .vuepress/highlight-rules.js
@@ -15,13 +15,13 @@ module.exports = {
 		greedy: true
 	},
 	'class-name': {
-		pattern: /((?:\b(?:Item|instanceof|as|new|val|var)\s+)|(?:catch\s+\())[\w.\\]+/i,
+		pattern: /((?:\b(?:instanceof|as|new|val|var)\s+)|(?:catch\s+\())[A-Z][\w.\\]+/,
 		lookbehind: true,
 		inside: {
 			punctuation: /[.\\]/
 		}
 	},
-	'constant': /\b(?:INCREASE|DECREASE|NEXT|PREVIOUS|ON|OFF|OPEN|CLOSED|PLAY|PAUSE|REWIND|FASTFORWARD|STOP|MOVE|UP|DOWN)\b/,
+	'constant': /\b(?:INCREASE|DECREASE|NEXT|PREVIOUS|ON|OFF|OPEN|CLOSED|PLAY|PAUSE|REWIND|FASTFORWARD|STOP|MOVE|UP|DOWN|string|int|boolean|long|float|short|char|byte)\b/,
 	'keyword': /\b(?:if|else|while|do|for|return|in|instanceof|import|function|new|try|throw|catch|finally|null|break|continue|switch|case|default|synchronized|rule|when|Item|Thing|Channel|changed|updated|Time cron|Time is midnight|Time is noon|System started|System shuts down|received update|received command|triggered|from|to|or|as|then|end|var|val)\b/,
 	'boolean': /\b(?:true|false)\b/,
 	'function': /[a-z0-9_]+(?=\()/i,
