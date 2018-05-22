500328b273d888aa874cac35014240b7fac4fdf1
Yannick Schaus <github@schaus.net>
2018-05-23 00:49:02 +0200

    Fix infinite loop in IconsetDisplay.vue
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/components/IconsetDisplay.vue
+++ .vuepress/components/IconsetDisplay.vue
@@ -3,24 +3,24 @@
     <h2>Places</h2>
     <div>
       <div class="iconset-icons">
-        <div class="iconset-icon" v-for="icon in categories.places.sort()">
-          <img :src="iconFile(icon)" :title="iconTooltip(icon)" />
+        <div class="iconset-icon" v-for="icon in existingIcons(categories.places).sort()">
+          <img :src="iconFile(icon)" />
           <caption>{{icon}}</caption>
         </div>
       </div>
     </div>
     <h2>Things</h2>
     <div class="iconset-icons">
-        <div class="iconset-icon" v-for="icon in categories.things.sort()">
+        <div class="iconset-icon" v-for="icon in existingIcons(categories.things).sort()">
           <img :src="iconFile(icon)" :title="iconTooltip(icon)" />
           <caption>{{icon}}</caption>
         </div>
     </div>
     <h2>Channels</h2>
-    <div v-for="(subcategoryicons,subcategory) in categories.channels">
+    <div v-for="subcategory in channelSubcategories">
       <h3>{{subcategory}}</h3>
       <div class="iconset-icons">
-        <div class="iconset-icon" v-for="icon in subcategoryicons.sort()">
+        <div class="iconset-icon" v-for="icon in existingIcons(categories.channels[subcategory]).sort()">
           <img :src="iconFile(icon)" :title="iconTooltip(icon)" />
           <caption>{{icon}}</caption>
         </div>
@@ -29,7 +29,7 @@
     <h2>Other Icons</h2>
     <div class="iconset-icons">
       <div class="iconset-icon" v-for="icon in icons_without_category.sort()">
-        <img :src="`/iconsets/classic/${icon}`" />
+        <img :src="`/iconsets/classic/${icon}`" :title="iconTooltip(icon.replace('.png', ''))" />
         <caption>{{icon.replace('.png', '')}}</caption>
       </div>
     </div>
@@ -66,22 +66,21 @@ export default {
   data () {
     return {
       allicons: null,
-      categories: null
+      categories: null,
+      channelSubcategories: null
     }
   },
   created () {
     allicons = this.icons.split(',')
     this.categories = this.$page.frontmatter.categories
-    // this.icons_without_category = this.allicons.filter(i => 
-    //   this.categories.places.indexOf(i + '.png') < 0 &&
-    //   this.categories.things.indexOf(i + '.png') < 0
-    //   //!Object.values(this.categories.channels).every(c => c.indexOf(i + '.png') < 0)
-    // )
+    this.channelSubcategories = Object.keys(this.categories.channels)
   },
   methods: {
+    existingIcons (category) {
+      return (category.filter(i => allicons.indexOf(i + '.png') >= 0))
+    },
     iconFile (icon) {
-      return (allicons.indexOf(icon + '.png') >= 0) ?
-        `/iconsets/classic/${icon}.png` : '/icon_no_category.png'
+      return `/iconsets/classic/${icon}.png`
     },
     iconTooltip (icon) {
       return (allicons.filter(i => i.indexOf(icon + '.png') === 0 || i.indexOf(icon + '-') === 0)).map(i => i.replace('.png', '')).join(' ')
