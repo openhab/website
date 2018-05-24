e5ec0a09a8b354b27c0e182d01b12c8588741bae
Yannick Schaus <github@schaus.net>
2018-05-24 20:44:01 +0200

    prepare-docs: Add prev links to sub-addons pages
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- prepare-docs.rb
+++ prepare-docs.rb
@@ -110,6 +110,9 @@ def process_file(indir, file, outdir, source)
                             end
 
                             out.puts "source: #{source}"
+
+                            # For sub-bundles, set the "prev" link to the main add-on
+                            out.puts "prev: ../#{addon.split('.')[0]}" if addon.include?('.')
                         end
                     end
 
