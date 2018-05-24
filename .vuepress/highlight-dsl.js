5f5a5cdc0420f3ecf53fbc9cce1f667aa1c115e2
Yannick Schaus <github@schaus.net>
2018-05-24 16:51:41 +0200

    Re-enable manual override copying
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/highlight-dsl.js
+++ .vuepress/highlight-dsl.js
@@ -22,7 +22,7 @@ module.exports = {
 		}
 	},
 	'constant': /\b(?:label|icon|item|mappings|item|minValue|maxValue|step|switchSupport|height|url|refresh|encoding|visibility|period|service|begin|end|legend|labelcolor|valuecolor|AVG|MIN|MAX|SUM|AND|OR|NAND|NOR|EQUAL)\b/,
-	'keyword': /\b(?:Color|Contact|DateTime|Dimmer|Group|Number|Player|Rollershutter|String|Switch|Location|Frame|Default|Text|Group|Selection|Setpoint|Slider|Colorpicker|Chart|Webview|Mapview|Image|Video|Item|Thing|Bridge|Channels|Time|Type|Sitemap|sitemap|Strategies|Items|Length|Temperature|Pressure|Speed|Intensity|Dimensionless|DimensionLess|Angle|Power|Energy)\b/,
+	'keyword': /\b(?:Color|Contact|DateTime|Dimmer|Group|Number|Player|Rollershutter|String|Switch|Location|Frame|Default|Text|Group|Selection|Setpoint|Slider|Colorpicker|Chart|Webview|Mapview|Image|Video|Item|Thing|Bridge|Channels|Time|Type|Sitemap|sitemap|Strategies|Items|Length|Temperature|Pressure|Speed|Intensity|Dimensionless|DimensionLess|Angle|Power|Energy|Illuminance)\b/,
 	'boolean': /\b(?:true|false)\b/,
 	'function': /\s<[a-z0-9_]+>/i,
   'tag': /\s\([a-z0-9_, ]+\)/i,
