d029bf77214ef639396cbae74d3ab94c299387fd
Yannick Schaus <github@schaus.net>
2018-06-08 14:49:49 +0200

    Add cookie banner in enhanceApp.js
    This will prevent having to rely on Netlify to inject
    the script before </body>.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -22,5 +22,20 @@ export default ({
           }
         }
       })
+
+      // Add the cookie consent before </body>
+      let tag = document.createElement('script')
+      tag.setAttribute('type', 'text/javascript')
+      tag.setAttribute('id', 'cookieinfo')
+      tag.setAttribute('src', '/cookieinfo.min.js')
+      tag.setAttribute('data-bg', '#645862')
+      tag.setAttribute('data-fg', '#FFFFFF')
+      tag.setAttribute('data-link', '#F1D600')
+      tag.setAttribute('data-linkmsg', 'Privacy Policy')
+      tag.setAttribute('data-moreinfo', '/privacy.html')
+      tag.setAttribute('data-cookie', 'CookieInfoScript')
+      tag.setAttribute('data-text-align', 'left')
+      tag.setAttribute('data-close-text', 'Got it!')
+      document.body.appendChild(tag)
     }
   }
