e4ed777906372813c60aa738d9246558bb541afe
Yannick Schaus <github@schaus.net>
2018-07-08 22:39:30 +0200

    Fixes
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- prepare-docs.rb
+++ prepare-docs.rb
@@ -194,7 +194,7 @@ def process_file(indir, file, outdir, source)
             # Handle obsolete bindings
             if in_frontmatter && (line =~ /label: / || line =~ /title: /) && outdir == 'addons/bindings' && file =~ /1\// then
                 addon = file.split('/')[0]
-                if Dir.exists?("#{indir}/#{addon.gsub('1', '')}") then
+                if !$ignore_bindings.include?(addon.gsub('1', '')) && Dir.exists?("#{indir}/#{addon.gsub('1', '')}") then
                     line = line.gsub("\n", "") + ' (1.x)' if !(line =~ /1\.x/)
                     if !obsolete_binding then
                         obsolete_binding = true
@@ -505,7 +505,7 @@ puts ">>> Migrating add-ons: Bindings"
 
 Dir.glob(".vuepress/openhab-docs/_addons_bindings/**") { |path|
     addon = File.basename(path)
-    break if $ignore_bindings.include?(addon)
+    next if $ignore_bindings.include?(addon)
     puts " -> #{addon}"
     FileUtils.mkdir_p("addons/bindings/" + addon)
     process_file(".vuepress/openhab-docs/_addons_bindings", addon + "/readme.md", "addons/bindings", nil)
