76522e599df4815c5a2181d887be8f8796357aa3
Yannick Schaus <ghys@users.noreply.github.com>
2019-01-12 14:42:37 +0100

    Fix edit link
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- prepare-docs.rb
+++ prepare-docs.rb
@@ -503,7 +503,7 @@ FileUtils.mkdir_p("docs/ecosystem/mycroft")
 FileUtils.mkdir_p("docs/ecosystem/google-assistant")
 process_file(".vuepress/openhab-docs/_addons_ios/alexa-skill", "readme.md", "docs/ecosystem/alexa", "https://github.com/openhab/openhab-alexa/blob/master/USAGE.md")
 process_file(".vuepress/openhab-docs/_addons_ios/mycroft-skill", "readme.md", "docs/ecosystem/mycroft", "https://github.com/openhab/openhab-mycroft/blob/master/USAGE.md")
-process_file(".vuepress/openhab-docs/_addons_ios/google-assistant", "readme.md", "docs/ecosystem/google-assistant", "https://github.com/openhab/openhab-google-assistant/blob/master/USAGE.md")
+process_file(".vuepress/openhab-docs/_addons_ios/google-assistant", "readme.md", "docs/ecosystem/google-assistant", "https://github.com/openhab/openhab-google-assistant/blob/master/docs/USAGE.md")
 FileUtils.cp_r(".vuepress/openhab-docs/_addons_ios/google-assistant/images", "docs/ecosystem/google-assistant")
 
 
