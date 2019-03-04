1ed4a8e439bd21c671800e561a7db27b06e45b5b
Yannick Schaus <ghys@users.noreply.github.com>
2019-03-05 00:09:25 +0100

    Fix iconset page (#162)
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- generate_iconset_doc.rb
+++ generate_iconset_doc.rb
@@ -19,7 +19,7 @@ $categories_channels = {}
 $categories_places = []
 $categories_thing = []
 
-Dir.glob("#{$original_iconsets_location}/#{$iconset_name}/icons/*.png").each { |path|
+Dir.glob("#{$original_iconsets_location}/#{$iconset_name}/src/main/resources/icons/*.png").each { |path|
     $icons_list.push(File.basename(path))
 }
 
@@ -72,6 +72,6 @@ File.open("#{$out_dir}/#{$iconset_name}/readme.md", "w+") { |f|
 puts " -> File written in #{$out_dir}/#{$iconset_name}/readme.md"
 
 # FileUtils.mkdir_p(".vuepress/public/iconsets")
-FileUtils.cp_r("#{$original_iconsets_location}/#{$iconset_name}/icons", ".vuepress/public/iconsets/#{$iconset_name}")
+FileUtils.cp_r("#{$original_iconsets_location}/#{$iconset_name}/src/main/resources/icons", ".vuepress/public/iconsets/#{$iconset_name}")
 
 puts " -> Icons copied to .vuepress/public/iconsets/#{$iconset_name}"
