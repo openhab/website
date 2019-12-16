38f65226e0e1c11c73c1631c56e5eb13b0b10449
Yannick Schaus <github@schaus.net>
2019-12-16 16:11:51 +0100

    Add snapshot v2.5 docs (#219)
    Update Previous Versions dropdown component
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

---
+++ .vuepress/public/v2.5/assets/js/70.0c321069.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[70],{481:function(t,n,e){"use strict";e.r(n);var o={mounted:function(){window.location.href=this.$page.frontmatter.redirect_to}},i=e(0),r=Object(i.a)(o,function(){var t=this.$createElement;return(this._self._c||t)("div")},[],!1,null,null,null);n.default=r.exports}}]);
