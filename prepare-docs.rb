b9f98cb8203808b0705666f652b7f1999477cb19
Yannick Schaus <github@schaus.net>
2020-02-10 20:01:37 +0100

    Update edit links for general docs (v2)
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- prepare-docs.rb
+++ prepare-docs.rb
@@ -8,7 +8,7 @@ require "rexml/document"
 # require "nokogiri"
 
 $docs_repo = "https://github.com/openhab/openhab-docs"
-$docs_repo_root = $docs_repo + "/blob/master"
+$docs_repo_root = $docs_repo + "/blob/2.5.x"
 $docs_repo_branch = "final"
 $addons_repo_branch = "2.5.x"
 $version = nil
@@ -286,7 +286,7 @@ def process_file(indir, file, outdir, source)
 end
 
 puts ">>> Migrating the introduction article"
-process_file(".vuepress/openhab-docs", "introduction.md", "docs", "https://github.com/openhab/openhab-docs/blob/master/introduction.md")
+process_file(".vuepress/openhab-docs", "introduction.md", "docs", "#{$docs_repo_root}/introduction.md")
 FileUtils.mv("docs/introduction.md", "docs/readme.md")
 
 
