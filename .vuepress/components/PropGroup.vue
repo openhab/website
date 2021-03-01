1bc34f9feede21612f10c9f6e1f0a734c1cf9f21
Yannick Schaus <github@schaus.net>
2021-03-01 15:43:39 +0100

    Add props description components (#276)
    Those component can be used to describe config parameters & groups.
    The UI component generated docs will use those but they could be useful
    for describing configuration of e.g. thing types, channels, profiles,
    rule modules...
    
    Minor dark mode style adjustments.
    Change wording of tip box in install instructions as per @mstormi's request.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

---
+++ .vuepress/components/PropGroup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="prop-group">
+    <!-- <h3 class="prop-group-header" :id="'prop-group-' + name">
+      <a :href="'#prop-group-' + name" class="header-anchor">#</a>
+      {{label}}
+    </h3> -->
+    <slot />
+  </div>
+</template>
+
+<style lang="stylus">
+h3.prop-group-header
+  margin-top -3.5rem
+  padding-top 4.6rem
+</style>
+
+<script>
+export default {
+  props: ['name', 'label']
+}
+</script>
---
+++ .vuepress/components/PropOption.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="prop-option">
+    <dt><code>{{value}}</code></dt>
+    <dd>{{label}}</dd>
+  </div>
+</template>
+
+<style lang="stylus">
+
+</style>
+
+<script>
+export default {
+  props: ['value', 'label']
+}
+</script>
