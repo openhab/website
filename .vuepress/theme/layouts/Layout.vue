4895fc7e01cbf385ca5079e3617016cadf074f41
Yannick Schaus <github@schaus.net>
2021-06-28 08:40:07 +0200

    Add banner for archived versions
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -3,6 +3,11 @@
         <template slot="sidebar-top">
             <doc-previous-versions />
         </template>
+        <template slot="page-top">
+            <div v-if="$site.themeConfig.docsVersion !== 'Stable' && $site.themeConfig.docsVersion !== 'Latest'" class="custom-block warning" style="margin: 4.6rem; margin-bottom: -4rem;">
+                <p>This page is part of archived documentation for openHAB <strong>{{$site.themeConfig.docsVersion}}</strong>. <a href="https://openhab.org/docs/">Go to the current stable version</a></p>
+            </div>
+        </template>
         <template slot="page-bottom">
             <Footer />
         </template>
