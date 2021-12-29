54da011f6c1037947b731d02547f690ca81f148c
Jerome Luckenbach <github@luckenba.ch>
2021-12-29 19:47:46 +0100

    Enhance rss sorting and dynamic feed meta (#329)
    * Add reversed by date sorting.
    
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>
    
    * Generate feed relevant meta entries for the blogposts.
    
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>

--- add-blog-meta.rb
+++ add-blog-meta.rb
@@ -12,12 +12,16 @@ Dir.glob("blog/*.md").each { |file|
     og_description = 'a vendor and technology agnostic open source automation software for your home'
     og_image = nil
 
+    #Feed Meta
+    fm_author = nil
+
     FileUtils.mkdir_p('.vuepress/tmp')
     FileUtils.mv(file, ".vuepress/tmp/#{File.basename(file)}")
 
     puts " -> #{file}"
     File.open(file, 'w+') { |out|
         File.open(".vuepress/tmp/#{File.basename(file)}").each { |line|
+
             # FIXME: require "yaml" and parse properly one day...
             if parsing_multiline_excerpt then
                 if line =~ /^  / then
@@ -28,9 +32,11 @@ Dir.glob("blog/*.md").each { |file|
                     parsing_multiline_excerpt = false
                 end
             end
+
             if in_frontmatter && line =~ /^title:/ then
                 og_title = line.gsub('title: ', '').gsub("\n", "")
             end
+
             if in_frontmatter && line =~ /^excerpt:/ then
                 og_description = line.gsub('excerpt: ', '').gsub("\n", "").gsub('[', '').gsub(']', '').gsub(/\(http[:\/\-0-9A-Za-z\.]+\)/, '')
                 if og_description == ">-" then
@@ -39,8 +45,13 @@ Dir.glob("blog/*.md").each { |file|
                     next
                 end
             end
+
             if in_frontmatter && line =~ /^previewimage:/ then
                 og_image = line.gsub('previewimage: ', '').gsub("\n", "")
+            end
+
+            if in_frontmatter && line =~ /^author:/ then
+                fm_author = line.gsub('author: ', '').gsub("\n", "")
             end
 
             if line =~ /^---$/ then
@@ -57,6 +68,16 @@ Dir.glob("blog/*.md").each { |file|
                     out.puts "    content: #{og_description}"
                     out.puts "  - property: og:image" if og_image
                     out.puts "    content: https://www.openhab.org#{og_image}" if og_image
+
+                    # Add feed meta tags, when something relevant is available
+                    if fm_author != nil || og_image != nil
+
+                        out.puts "feed:"
+                        out.puts "  image: https://www.openhab.org#{og_image}" if og_image
+                        out.puts "  author:"
+                        out.puts "    - "
+                        out.puts "      name: #{fm_author}"
+                    end
                     
                     in_frontmatter = false
                     frontmatter_processed = true
