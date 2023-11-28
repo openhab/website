openhab/website

commit 826055e6ec8b412b57d6bc3dc1c80f5d7fb807a9

Fix #425: The download page shows a snapshot download link when choosing Milestone (#426)
Not sure why this wasn't merged as it's a pretty major issue ;)

---------

Signed-off-by: Yannick Schaus <github@schaus.net>

.vuepress/components/InstallInstructions.vue
@@ -158,13 +158,13 @@ usermod -a -G openhab myownuser
         </div>
         <div v-if="selectedVersion === 'snapshot'">
             <div class="download-button-container">
-              <a class="download-button" style="margin-bottom: 0" :href="`https://ci.openhab.org/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab-addons/target/openhab-addons-4.1.0-SNAPSHOT.kar`" download>Download openHAB {{$page.frontmatter.currentSnapshotVersion}} Add-ons</a>
+              <a class="download-button" style="margin-bottom: 0" :href="addonsDownloadLink" download>Download openHAB {{$page.frontmatter.currentSnapshotVersion}} Add-ons</a>
             </div>
         </div>
       </ol>
     </div>
 
-    <div v-if="selectedSystem !== 'docker' && selectedVersion === 'stable'">
+    <div v-if="selectedSystem !== 'docker'">
       <hr>
       <h3>{{optionNumber('manual')}}Manual Installation</h3>
       <ol>
@@ -192,23 +192,6 @@ usermod -a -G openhab myownuser
           and wait for openHAB to perform its initial startup (this can take a few minutes depending on your machine)</li>
         <li>Navigate with a web browser to <code>http://{{selectedSystem === 'apple' || selectedSystem === 'win10' ? 'localhost' : '&lt;ip-address&gt;'}}:8080</code></li>
         <li>Continue by following the <router-link to="/docs/tutorial/">tutorial</router-link> to get started</li>
-      </ol>
-    </div>
-
-    <div v-if="selectedSystem !== 'docker' && (selectedVersion === 'testing' || selectedVersion === 'snapshot')">
-      <hr />
-      <h3>Manual Installation</h3>
-      <ol>
-        <li>Install a recent Java 17 platform (we recommend <a target="_blank" href="https://www.azul.com/downloads/zulu-community/?version=java-17-lts&package=jdk">the Zulu builds of OpenJDK</a>)</li>
-        <li>Download and extract the distribution from <a href="https://ci.openhab.org/">https://ci.openhab.org/</a>:</li>
-        <div class="download-button-container">
-          <a target="_blank" class="download-button big" :href="`https://ci.openhab.org/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab/target/openhab-4.1.0-SNAPSHOT.zip`" download>Latest openHAB {{$page.frontmatter.currentSnapshotVersion}} Build</a>
-        </div>
-        <li><strong>(Optional)</strong> Download the add-on archives for offline use and put them in the <code>addons</code> folder of the extracted distribution:</li>
-        &#128712; <small>	You don't need the add-ons archives if your machine has Internet access, openHAB will download add-ons you need online as necessary.</small>
-        <div class="download-button-container">
-          <a class="download-button" style="margin-bottom: 0" :href="`https://ci.openhab.org/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab-addons/target/openhab-addons-4.1.0-SNAPSHOT.kar`" download>Download openHAB {{$page.frontmatter.currentSnapshotVersion}} Add-ons</a>
-        </div>
       </ol>
     </div>
 
@@ -419,6 +402,8 @@ export default {
         return `https://github.com/openhab/openhab-distro/releases/download/${this.$page.frontmatter.currentVersion}/openhab-${this.$page.frontmatter.currentVersion}.zip`
       } else if (this.selectedVersion === 'testing') {
         return `https://github.com/openhab/openhab-distro/releases/download/${this.$page.frontmatter.currentMilestoneVersion}/openhab-${this.$page.frontmatter.currentMilestoneVersion}.zip`
+      } else {
+        return `https://ci.openhab.org/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab/target/openhab-${this.$page.frontmatter.currentSnapshotVersion}.kar`
       }
     },
     addonsDownloadLink () {
@@ -426,6 +411,8 @@ export default {
         return `https://github.com/openhab/openhab-distro/releases/download/${this.$page.frontmatter.currentVersion}/openhab-addons-${this.$page.frontmatter.currentVersion}.kar`        
       } else if (this.selectedVersion === 'testing') {
         return `https://github.com/openhab/openhab-distro/releases/download/${this.$page.frontmatter.currentMilestoneVersion}/openhab-addons-${this.$page.frontmatter.currentMilestoneVersion}.kar`
+      } else {
+        return `https://ci.openhab.org/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab-addons/target/openhab-addons-${this.$page.frontmatter.currentSnapshotVersion}.kar`
       }
     },
     currentDownloadVersion () {

commit dc75d8c45f9b7084c45360e03c40f000842314f5

Fix download link for runtime
This fixes a regression introduced by https://github.com/openhab/website/pull/426

Signed-off-by: Kai Kreuzer <kai@openhab.org>

.vuepress/components/InstallInstructions.vue
@@ -403,7 +403,7 @@ export default {
       } else if (this.selectedVersion === 'testing') {
         return `https://github.com/openhab/openhab-distro/releases/download/${this.$page.frontmatter.currentMilestoneVersion}/openhab-${this.$page.frontmatter.currentMilestoneVersion}.zip`
       } else {
-        return `https://ci.openhab.org/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab/target/openhab-${this.$page.frontmatter.currentSnapshotVersion}.kar`
+        return `https://ci.openhab.org/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab/target/openhab-${this.$page.frontmatter.currentSnapshotVersion}.zip`
       }
     },
     addonsDownloadLink () {