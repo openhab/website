openhab/website

commit 9ddc87f242784b60156c078f893668657d92b836

Improve add-on README logo styling (#488)
Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

.vuepress/components/AddonLogo.vue
@@ -8,6 +8,7 @@
     float right
     margin-left 4rem
 .addon-logo
+  margin-top -3rem
   width 200px
   height 200px
   object-fit contain

commit b83bb2e520fb95aea3c7a8a23b179378fee133c1

Fix style issue with add-on README logo on mobile
Regression from #488.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

.vuepress/components/AddonLogo.vue
@@ -7,8 +7,8 @@
   .addon-logo
     float right
     margin-left 4rem
+    margin-top -3.5rem
 .addon-logo
-  margin-top -3rem
   width 200px
   height 200px
   object-fit contain